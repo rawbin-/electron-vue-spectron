5c82ebf0be67b7f0668c16a30c1c799c56f6c406
zhangliao <zhangliao@baiwang.com>
2019-11-14 16:36:06 +0800

    update to electron 6.1.4 + spectron 8.0.0 and e2e test does not work

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -5,6 +5,13 @@ describe('Launch', function () {
   afterEach(utils.afterEach)
 
   it('shows the proper application title', function () {
+    const app = this.app
+    console.log('test app.client:', app.client)
+    console.log('test app.electron:', app.electron)
+    console.log('test app.browserWindow:', app.browserWindow)
+    console.log('test app.webContents:', app.webContents)
+    console.log('test app.mainProcess:', app.mainProcess)
+    console.log('test app.rendererProcess:', app.rendererProcess)
     return this.app.client.getTitle()
       .then(title => {
         expect(title).to.equal('electron-vue-spectron')
